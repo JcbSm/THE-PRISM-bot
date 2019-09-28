c73ad5e12a06ffcd584eda22d890658354aa1c09
Jacob <jacob.ted.seaman@gmail.com>
2019-09-28 17:05:34 +0100

    Added some more Joinroles

--- commands/Utilities/joinrole.js
+++ commands/Utilities/joinrole.js
@@ -1,5 +1,7 @@
 const { Command } = require('discord-akairo');
 const roles = require('../../datafiles/self-roles.js')
+const color = require('../../datafiles/colors.json');
+const Discord = require('discord.js');
 
 class JoinRoleCommand extends Command {
     constructor() {
@@ -21,17 +23,41 @@ class JoinRoleCommand extends Command {
 
     async exec(message, args) {
 
-        const role = (await message.guild.fetchMembers()).roles.find(r => r.name.toLowerCase().includes(args.role.toLowerCase()))
-        if(!role) return message.reply('No role found.')
+        if(message.guild.id !== '447504770719154192') return message.reply('That Command only works in PRISM')
 
-        let joinableRole = roles.find(r => r.id == role.id)
+        if(!args.role) {
 
-        if(!joinableRole) return message.reply(`You are not allowed to give yourself this role`)
-        
-        if(message.member.roles.has(role.id)) {
-            message.reply('You already have this role.')
+            let gameRoles = roles.filter(r => r.category == 'Games')
+            let otherRoles = roles.filter(r => r.category === null)
+            console.log(gameRoles)
+
+            try{let selfRoleEmbed = new Discord.RichEmbed()
+
+                .setTitle('Joinable Roles in PRISM')
+                .setFooter('Type \"-joinRole <role>\" to get the role.')
+                .setColor(color.purple)
+                .addField('**Game Roles**', gameRoles.map(r => r.name))
+                .addField('**Other Roles**', otherRoles.map(r => r.name))
+
+            message.channel.send(selfRoleEmbed)
+            }catch(error)
+            {console.log(error)}
+
         } else {
-        message.member.addRole(role.id).then(message.channel.send(`***Successfully given you the ${role.name} role.***`))
+
+            const role = (await message.guild.fetchMembers()).roles.find(r => r.name.toLowerCase().includes(args.role.toLowerCase()))
+            if(!role) return message.reply('No role found.')
+
+            let joinableRole = roles.find(r => r.id == role.id)
+
+            if(!joinableRole) return message.reply(`You are not allowed to give yourself this role`)
+            
+            if(message.member.roles.has(role.id)) {
+                message.reply('You already have this role.')
+            } else {
+            message.member.addRole(role.id).then(message.channel.send(`***Successfully given you the ${role.name} role.***`))
+            }
+
         }
     }
 }
--- datafiles/self-roles.js
+++ datafiles/self-roles.js
@@ -1,19 +1,33 @@
 module.exports = [
     {
         name: "Events",
-        id: "578628568314150913"
+        id: "578628568314150913",
+        category: null
     },
     {
         name: "Film Nights",
-        id: "578840281005424640"
+        id: "578840281005424640",
+        category: null
     },
     {
         name: 'Revision',
-        id: '552219451484012558'
+        id: '552219451484012558',
+        category: null
     },
     {
         name: 'Minecraft',
-        id: '603598424448958468'
+        id: '603598424448958468',
+        category: 'Games'
+    },
+    {
+        name: 'Insurgency',
+        id: '627532049183670282',
+        category: 'Games',
+    },
+    {
+        name: 'Garry\'s Mod',
+        id: '627532097401389057',
+        category: 'Games'
     }
 
 ]
