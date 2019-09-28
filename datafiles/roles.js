4d6924fc37e100cddfff6fa1e6b86a86e37eb404
Jacob <jacob.ted.seaman@gmail.com>
2019-09-28 19:43:13 +0100

    A

---
+++ datafiles/roles.js
@@ -0,0 +1,132 @@
+module.exports = [
+
+    //Special
+    {
+        name: "YouTube",
+        id: "477483614259511317",
+        category: 'Sepcial'
+    },
+    {
+        name: 'Twtich',
+        id: '477483812469735434',
+        category: 'Special'
+    },
+    {
+        name: 'Winner',
+        id: '577078556509601802',
+        category: 'Special'
+    },
+    {
+       name: 'Mr Beast Counter [10k]',
+       id: '627537528395923469',
+       category: 'Special' 
+    },
+    {
+        name: 'Salesman',
+        id: '548268779919114250',
+        category: 'Specials'
+    },
+
+    //Levels
+    {
+        name: '[40] Octahedral',
+        id: '577148530309857281',
+        category: 'Levels'
+    },
+    {
+        name: '[30] Tetrahedral',
+        id: '575350995555713035',
+        category: 'Levels'
+    },
+    {
+        name: '[20] Octagonal',
+        id: '575350091322490880',
+        category: 'Levels'
+    },
+    {
+        name: '[10] Square',
+        id: '518870362784792600',
+        category: 'Levels'
+    },
+    {
+        name: '[0] Triangular',
+        id: '447508711855095808',
+        category: 'Levels'
+    },
+
+    //Games
+    {
+        name: 'Minecraft',
+        id: '603598424448958468',
+        category: 'Games'
+    },
+    {
+        name: 'Insurgency',
+        id: '627532049183670282',
+        category: 'Games',
+    },
+    {
+        name: 'Garry\'s Mod',
+        id: '627532097401389057',
+        category: 'Games'
+    },
+
+    //Economy
+    {
+        name: 'SpaceX CEO',
+        id: '448514425184452609',
+        category: 'Economy'
+    },
+    {
+        name: 'Cam Girl',
+        id: '448512063279595520',
+        category: 'Economy'
+    },
+    {
+        name: 'Bitcoin Miner',
+        id: '448510331048689677',
+        category: 'Economy'
+    },
+    {
+        name: 'Uber Driver',
+        id: '448512062310711306',
+        category: 'Economy'
+    },
+    {
+        name: 'Cleaner',
+        id: '448512060595109889',
+        category: 'Economy'
+    },
+    {
+        name: 'Day Job',
+        id: '448169060572594176',
+        category: 'Economy'
+    },
+    //Other Roles
+    {
+        name: "Events",
+        id: "578628568314150913",
+        category: null
+    },
+    {
+        name: "No XP",
+        id: '575361435966767134',
+        category: null
+    },
+    {
+        name: "Film Nights",
+        id: "578840281005424640",
+        category: null
+    },
+    {
+        name: 'Revision',
+        id: '552219451484012558',
+        category: null
+    },
+    {
+        name: 'Allow Mention',
+        id: '611697365316927570',
+        category: null
+    }
+
+]
