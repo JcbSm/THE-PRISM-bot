898186f30e05fecca0f61d5ab8dad2574ae6e7c7
Jacob <jacob.ted.seaman@gmail.com>
2020-06-28 04:02:12 +0100

    Create stop.js

---
+++ commands/stop.js
@@ -0,0 +1,25 @@
+const { Command } = require('discord-akairo');
+
+class ShutdownCommand extends Command {
+    constructor() {
+        super('shutdown', {
+            aliases: ['shutdown', 'stop'],
+            description: {
+                content: 'Stops the bot',
+                usage: 'shutdown'
+            },
+            ownerOnly: true,
+        })
+    }
+
+    async exec(message) {
+
+        if(message.author.id !== '227848397447626752') return;
+        
+        console.log(`Process remotely shut down by ${message.author.tag} in ${message.guild}`)
+        await message.channel.send(`Putting ${this.client.user} to sleep...`)
+        await this.client.destroy()
+    }
+}
+
+module.exports = ShutdownCommand;
