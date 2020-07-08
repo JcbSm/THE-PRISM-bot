c9520f38bdbd84f7166f9308ca4db6fbaa5fc8b4
Jacob <jacob.ted.seaman@gmail.com>
2020-07-08 15:01:26 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -10,4 +10,9 @@ module.exports = {
         let arr = link.split('/').slice(Math.max(link.split('/').length - 2, 0));
         return await (await client.channels.fetch(arr[0])).messages.fetch(arr[1]);
     },
+
+    getTime: function getTime(date) {
+        if(date === undefined) {date = new Date()}
+        return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    },
 }
