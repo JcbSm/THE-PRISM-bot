685b514667c9fa05a1857a6903b2dd8c7fa91d45
Jacob <jacob.ted.seaman@gmail.com>
2020-07-08 19:14:38 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,29 @@
 module.exports = {
 
+    pad: function pad(number, digits) {
+        Number(number); Number(digits);
+        if(number<0) return;
+        let n = number;
+        if(n === 0) {
+            ++n;
+        }
+        let count = 0;
+        if(n >= 1) ++count;
+        while (n/10 >= 1) {
+            n /= 10;
+            ++count;
+        }
+        let diff;
+        diff = digits - count;
+        if (diff < 0 ) return;
+        let numArray = number.toString().split("");
+        for (i = 0; i < diff; i++) {
+
+            numArray.unshift('0');
+        }
+        return numArray.join('')
+    },
+    
     rgb: function rgb(inputColor) {
 
         const Color = require('color')
@@ -13,6 +37,10 @@ module.exports = {
 
     getTime: function getTime(date) {
         if(date === undefined) {date = new Date()}
-        return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+        return `${module.exports.pad(date.getHours(), 2)}:${module.exports.pad(date.getMinutes(), 2)}:${module.exports.pad(date.getSeconds(), 2)}`
+    },
+    getUTCTime: function getTime(date) {
+        if(date === undefined) {date = new Date()}
+        return `${module.exports.pad(date.getUTCHours(), 2)}:${module.exports.pad(date.getUTCMinutes(), 2)}:${module.exports.pad(date.getUTCSeconds(), 2)}`
     },
 }
