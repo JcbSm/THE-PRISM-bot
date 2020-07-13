f2f05dc9c68fb209c8156842fae136a411f5befc
Jacob <jacob.ted.seaman@gmail.com>
2020-07-13 01:06:17 +0100

    Create allowText.js

---
+++ commands/Utilities/Private Call/allowText.js
@@ -0,0 +1,59 @@
+const { Command } = require('discord-akairo');
+
+class AllowTextClass extends Command {
+    constructor() {
+        super('allowText', {
+            aliases: ['allowText'],
+            args: [
+                {
+                    id: 'member',
+                    type: 'member',
+                    match: 'rest'
+                }
+            ],
+            category: 'private calls'
+        })
+    }
+
+    async exec(message, args) {
+
+        try{        
+        
+            if(message.guild.id === '447504770719154192' || message.guild.id === '569556194612740115') {
+    
+                const guild = message.guild
+    
+                if(message.channel.topic.split(';').shift() !== 'PRIVATE CALL') message.reply('This is not a private call text channel, please either make one or use an existing one.');
+                if(message.channel.topic.split(';').shift() === 'PRIVATE CALL') {
+                    const textChannel = message.channel
+                    const perms = textChannel.permissionOverwrites.filter(p => p.type === 'member')
+    
+                    if(perms.get(args.member.id)) {
+                        if(perms.get(args.member.id).allow.serialize().VIEW_CHANNEL){
+                            await textChannel.createOverwrite(args.member.id, { VIEW_CHANNEL: false })
+                            message.channel.send(`***${textChannel.name} is now invisible to ${args.member}***`)
+                        } else {
+                            await textChannel.createOverwrite(args.member.id, { VIEW_CHANNEL: true })
+                            message.channel.send(`***${textChannel.name} is now visible to ${args.member}***`)
+                        }
+                    } else if(textChannel.permissionOverwrites.get(message.guild.roles.cache.find(r => r.name === '@everyone').id).allow.serialize().VIEW_CHANNEL) {
+    
+                        await textChannel.createOverwrite(args.member.id, { VIEW_CHANNEL: false })
+                        message.channel.send(`***${textChannel.name} is now invisible to ${args.member}***`)
+    
+                    } else {
+                        await textChannel.createOverwrite(args.member.id, { VIEW_CHANNEL: true })
+                        message.channel.send(`***${textChannel.name} is now visible to ${args.member}***`)
+                    }
+    
+    
+                }
+    
+            }
+        
+        }catch(error){console.log(error)}
+    }
+
+}
+
+module.exports = AllowTextClass;
