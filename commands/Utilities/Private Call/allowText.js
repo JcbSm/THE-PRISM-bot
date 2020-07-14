f450f7917277b0de2353c15a15710dde2f2e8679
Jacob <jacob.ted.seaman@gmail.com>
2020-07-14 17:13:17 +0100

    Update allowText.js

--- commands/Utilities/Private Call/allowText.js
+++ commands/Utilities/Private Call/allowText.js
@@ -27,23 +27,26 @@ class AllowTextClass extends Command {
                 if(message.channel.topic.split(';').shift() === 'PRIVATE CALL') {
                     const textChannel = message.channel
                     const perms = textChannel.permissionOverwrites.filter(p => p.type === 'member')
-    
+
+                    if(!args.member) return message.reply('Please provide a member')
+                    if(message.channel.topic.includes(args.member.user.id)) return message.reply('You can\'t kick the owner')
+
                     if(perms.get(args.member.id)) {
                         if(perms.get(args.member.id).allow.serialize().VIEW_CHANNEL){
                             await textChannel.createOverwrite(args.member.id, { VIEW_CHANNEL: false })
-                            message.channel.send(`***${textChannel.name} is now invisible to ${args.member}***`)
+                            message.channel.send(`***${textChannel.name} is now invisible to ${args.member.user.tag}***`)
                         } else {
                             await textChannel.createOverwrite(args.member.id, { VIEW_CHANNEL: true })
-                            message.channel.send(`***${textChannel.name} is now visible to ${args.member}***`)
+                            message.channel.send(`***${textChannel.name} is now visible to ${args.member.user.tag}***`)
                         }
                     } else if(textChannel.permissionOverwrites.get(message.guild.roles.cache.find(r => r.name === '@everyone').id).allow.serialize().VIEW_CHANNEL) {
     
                         await textChannel.createOverwrite(args.member.id, { VIEW_CHANNEL: false })
-                        message.channel.send(`***${textChannel.name} is now invisible to ${args.member}***`)
+                        message.channel.send(`***${textChannel.name} is now invisible to ${args.member.user.tag}***`)
     
                     } else {
                         await textChannel.createOverwrite(args.member.id, { VIEW_CHANNEL: true })
-                        message.channel.send(`***${textChannel.name} is now visible to ${args.member}***`)
+                        message.channel.send(`***${textChannel.name} is now visible to ${args.member.user.tag}***`)
                     }
     
     
