31f347f3ddc9fbef37239a8c3458f6216b788f6c
Jacob <jacob.ted.seaman@gmail.com>
2020-07-29 16:21:17 +0100

    Update userLeave.js

--- listeners/Utilities/Private Calls/userLeave.js
+++ listeners/Utilities/Private Calls/userLeave.js
@@ -16,18 +16,38 @@ class PrivateCallLeaveListener extends Listener {
 
                 const guild = oldState.guild
                 const pChannels = await guild.channels.cache.filter(c => c.type === 'voice' && c.name.startsWith('🔒'))
+                let tChannel;
+                
+                if(!oldState.channel) {
 
-                if(!oldState.channel) return;
+                } else {
 
-                if(oldState.channel !== newState.channel && pChannels.map(m => m.id).includes(oldState.channel.id)) {
+                    tChannel = await guild.channels.cache.find(c => c.type === 'text' && c.topic && c.topic.startsWith('PRIVATE CALL') && c.topic.split(';').pop() === oldState.channel.id)
+    
+                    if(oldState.channel !== newState.channel) {
+    
+                        if(pChannels.map(m => m.id).includes(oldState.channel.id) && (await oldState.channel.members.map(m => m.id)).length == 0) {
+    
+                            async function tenSeconds (n, end, client) {
 
-                    if((await oldState.channel.members.map(m => m.id)).length == 0) {
+                                if(n < end) setTimeout(async function() {
 
-                        const tChannel = await guild.channels.cache.find(c => c.type === 'text' && c.topic && c.topic.startsWith('PRIVATE CALL') && c.topic.split(';').pop() === oldState.channel.id)
+                                    if((await client.channels.fetch(oldState.channel.id)).members.size === 0) {
+                                        tenSeconds(n, end, client);
+                                    }
+                                    if(n === end && (await client.channels.fetch(oldState.channel.id)).members.size === 0) {
+                                        await oldState.channel.delete();
+                                        await tChannel.delete();
+                                    }
 
-                        await oldState.channel.delete()
-                        await tChannel.delete()
+                                }, 1000);
+                    
+                                n++;
+                            };
 
+                            tenSeconds(0, 60, this.client)
+    
+                        }
                     }
                 }           
             }
