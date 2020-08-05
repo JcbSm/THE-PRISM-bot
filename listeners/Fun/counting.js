e0737dc004c8eca56609942768efe79b4e00a906
Jacob <jacob.ted.seaman@gmail.com>
2020-08-05 20:59:07 +0100

    Update counting.js

--- listeners/Fun/counting.js
+++ listeners/Fun/counting.js
@@ -15,7 +15,7 @@ class CountingListener extends Listener {
         if(message.channel.id !== config.prism.guild.channelIDs.counting) return;
         const lastCount = (await message.channel.messages.fetch( {limit:2} )).last()
         let valid;
-        if(/^\d{1,}$/.test(message.content)) {
+        if(/^[1-9]\d*$/.test(message.content)) {
             valid = true
         }
         if(Number(message.content) !== Number(lastCount.content)+1 || message.author.id === lastCount.author.id || !valid) return message.delete();
