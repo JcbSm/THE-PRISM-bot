32ed8ae0d1783f0a07890bc0bc6ff861a3d7620c
Jacob <jacob.ted.seaman@gmail.com>
2020-08-06 15:40:16 +0100

    Update um.js

--- listeners/Fun/Games/um.js
+++ listeners/Fun/Games/um.js
@@ -21,17 +21,37 @@ class UmListener extends Listener {
                 limit: 100
             })
 
+            let firstMessage;
+            let highScoreFirstMessage;
+
             const scoreMessage = await linkToMessage('https://discordapp.com/channels/447504770719154192/740196706058108958/740558584940593182', this.client)
             const highScoreMessage = await linkToMessage('https://discordapp.com/channels/447504770719154192/740196706058108958/740557623845322752', this.client)
+
             let score = Number(scoreMessage.content.split("\`")[1])
+            firstMessage = await linkToMessage(scoreMessage.embeds[0].description.slice(7,-1), this.client)
+
             let highScore = Number(highScoreMessage.content.split('\`')[1])
+            highScoreFirstMessage = await linkToMessage(highScoreMessage.embeds[0].description.slice(7,-1), this.client)
 
             const lastMessage = messages.filter(m => !m.author.bot).array()[1]
             let messagesArray = messages.map(m => m.content.toLowerCase())
             messagesArray.shift()
             messagesArray.splice(messagesArray.findIndex(m => m.startsWith('<')), 100)
 
-            //console.log(messagesArray)
+            function fail(message, score, high) {
+
+                let failMessage;
+                message.react('730846979977904218')
+                if(score == high) {
+
+                    failMessage = `You failed, **NEW HIGH SCORE:** \`${score}\``
+                } else {
+                    failMessage = `You failed, **SCORE:** \`${score}\``
+                }
+                message.reply(failMessage)
+                
+            }
+
 
             if(/^[a-z]{1,}$/i.test(message.content) === false || message.author.id === lastMessage.author.id) {
                 message.delete()
@@ -39,8 +59,7 @@ class UmListener extends Listener {
 
                 if(!words[message.content.toLowerCase()]) {
 
-                    message.react('730846979977904218')
-                    message.reply(`You failed, **SCORE:** \`${score}\``)
+                    fail(message, score, highScore)
                     score = 0;
                     //console.log('Not a word')
 
@@ -48,14 +67,12 @@ class UmListener extends Listener {
                 
                     if(messagesArray[0].trim().toLowerCase().charAt(messagesArray[0].length - 1) !== message.content.trim().toLowerCase().charAt(0)) {
 
-                        message.react('730846979977904218')
-                        message.reply(`You failed, **SCORE:** \`${score}\``)
+                        fail(message, score, highScore)
                         score = 0;
                         //console.log('No match')
                     } else if(messagesArray.includes(message.content.toLowerCase())) {
 
-                        message.react('730846979977904218')
-                        message.reply(`You failed, **SCORE:** \`${score}\``)
+                        fail(message, score, highScore)
                         score = 0;
                         //console.log('Repetition')
                     } else {
@@ -64,6 +81,7 @@ class UmListener extends Listener {
                         score++
                         if(score > highScore) {
                             highScore = score
+                            highScoreFirstMessage = firstMessage
                         }
     
                         if(lastMessage.content.trim().toLowerCase().split("").reverse().join("") == message.content.trim().toLowerCase()) {
@@ -76,9 +94,8 @@ class UmListener extends Listener {
 
                     message.react('✅')
                     score++
-                    if(score > highScore) {
-                        highScore = score
-                    }
+
+                    firstMessage = message
 
                     if(lastMessage.content.trim().toLowerCase().split("").reverse().join("") == message.content.trim().toLowerCase()) {
 
@@ -86,8 +103,16 @@ class UmListener extends Listener {
                     }
                 }
 
-                scoreMessage.edit(`**CURRENT SCORE**: \`${score}\``)
-                highScoreMessage.edit(`**HIGHEST SCORE**: \`${highScore}\``)
+                scoreMessage.edit(`**CURRENT SCORE**: \`${score}\``, {embed: {
+
+                    type: 'rich',
+                    description: `[Jump](${firstMessage.url})`
+                }})
+                highScoreMessage.edit(`**HIGHEST SCORE**: \`${highScore}\``, {embed: {
+
+                    type: 'rich',
+                    description: `[Jump](${highScoreFirstMessage.url})`
+                }})
             }
 
         } catch(e) {
