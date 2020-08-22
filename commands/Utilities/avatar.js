3ad9cf6623da01d6285d7f5e5c2016b9d0c355de
Jacob <jacob.ted.seaman@gmail.com>
2020-08-22 16:32:08 +0100

    Update avatar.js

--- commands/Utilities/avatar.js
+++ commands/Utilities/avatar.js
@@ -1,6 +1,5 @@
 const { Command } = require('discord-akairo');
 const { colors } = require('../../config');
-const { rgb } = require('../../functions')
 
 class AvatarCommand extends Command {
     constructor() {
@@ -21,43 +20,16 @@ class AvatarCommand extends Command {
     }
 
     exec(message, args) {
-        if(!args.member) {
 
-            //check for avatar
-            if(!message.author.avatarURL) {
-                return message.reply('No avatar found.')
-            }
+        const member = args.member ? args.member : message.member
 
-            //No user targeted, own avatar shown            
-            message.channel.send({ embed: {
+        message.channel.send({ embed: {
 
-                type: 'rich',
-                image: {
-                    url: message.author.avatarURL({size: 4096})
-                },
-                footer: {
-                    text: 'Couldn\'t find a target user, displaying your own.'
-                },
-                color: rgb(colors.purple)
-
-            }});
-        } else {
-
-            //check for avatar
-            if(!args.member.user.avatarURL) {
-                return message.reply('No avatar found.')
-            }
-
-            //Shows avatar of targeted user
-            message.channel.send({ embed: {
-
-                type: 'rich',
-                image: {
-                    url: args.member.user.displayAvatarURL({size: 4096})
-                },
-                color: rgb(colors.purple)
-            }});
-        }
+            type: 'rich',
+            image: { url: member.user.displayAvatarURL({size: 4096}) },
+            color: colors.purple
+            
+        }})
     }
 }
 
