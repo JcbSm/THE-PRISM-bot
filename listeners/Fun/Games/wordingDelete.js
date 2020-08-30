6064a56b91437c234f6f00031b56f4b9554f49e4
Jacob <jacob.ted.seaman@gmail.com>
2020-08-30 01:15:48 +0100

    Update wordingDelete.js

--- listeners/Fun/Games/wordingDelete.js
+++ listeners/Fun/Games/wordingDelete.js
@@ -16,19 +16,29 @@ class WordingDeleteListener extends Listener {
 
             if(message.channel.id !== prism.guild.channelIDs.wording) return;
 
-            console.log("MESSAGE DELETED IN WORDING")
+            // console.log("MESSAGE DELETED IN WORDING")
 
-            const scoreMessage = await linkToMessage('https://discordapp.com/channels/447504770719154192/740196706058108958/740558584940593182', this.client)
-            const highScoreMessage = await linkToMessage('https://discordapp.com/channels/447504770719154192/740196706058108958/740557623845322752', this.client)
+            const [scoreMessage, highScoreMessage] = [
+                await linkToMessage('https://discordapp.com/channels/447504770719154192/740196706058108958/749406792282538035', this.client),
+                await linkToMessage('https://discordapp.com/channels/447504770719154192/740196706058108958/749406778810433556', this.client)
+            ];
 
-            let score = Number(scoreMessage.embeds[0].title.split("\`")[1])
-            let firstMessage = await linkToMessage(scoreMessage.embeds[0].description.slice(7,-1), this.client)
+            let [score, highScore] = [
+                Number(scoreMessage.embeds[0].title.split("\`")[1]),
+                Number(highScoreMessage.embeds[0].title.split("\`")[1])
+            ]
 
-            let highScore = Number(highScoreMessage.embeds[0].title.split('\`')[1])
-            let highScoreFirstMessage = await linkToMessage(highScoreMessage.embeds[0].description.slice(7,-1), this.client)
-
+            let [firstMessage, highScoreFirstMessage] = [
+                await linkToMessage(scoreMessage.embeds[0].description.slice(7,-1), this.client),
+                await linkToMessage(highScoreMessage.embeds[0].description.slice(7,-1), this.client)
+            ];
             
-            let changedScore = emoji.numbers[message.reactions.cache.last().emoji.name]
+            const messages = await message.channel.messages.fetch({
+
+                limit: 100
+            })
+
+            //let changedScore = emoji.numbers[message.reactions.cache.last().emoji.name]
 
             scoreMessage.edit({ embed: {
 
@@ -53,4 +63,4 @@ class WordingDeleteListener extends Listener {
     }
 }
 
-module.exports = WordingDeleteListener;
+//module.exports = WordingDeleteListener;
