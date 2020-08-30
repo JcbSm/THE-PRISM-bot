d9b0e25080537bcad9928fb7e0508f6fd1510530
Jacob <jacob.ted.seaman@gmail.com>
2020-08-30 01:15:38 +0100

    Update wording.js

--- listeners/Fun/Games/wording.js
+++ listeners/Fun/Games/wording.js
@@ -1,7 +1,7 @@
 const { Listener } = require('discord-akairo');
 const { prism, emoji } = require('../../../config')
 const { linkToMessage, alphabetical } = require('../../../functions')
-const words = require('./words_dictionary.json')
+const words = require('./words_dictionary.json');
 
 class WordingListener extends Listener {
     constructor() {
@@ -14,117 +14,127 @@ class WordingListener extends Listener {
 
         try{
 
-            function fail(message, score, high) {
+            if(message.channel.id !== prism.guild.channelIDs.wording || message.author.bot) return;
+            
+            // Define Variables
 
-                let failMessage;
-                message.react('730846979977904218')
-                if(score == high) {
+            const [scoreMessage, highScoreMessage] = [
+                await linkToMessage('https://discordapp.com/channels/447504770719154192/740196706058108958/749406792282538035', this.client),
+                await linkToMessage('https://discordapp.com/channels/447504770719154192/740196706058108958/749406778810433556', this.client)
+            ];
 
-                    failMessage = `You failed, **NEW HIGH SCORE:** \`${score}\``
-                } else {
-                    failMessage = `You failed, **SCORE:** \`${score}\``
-                }
-                message.reply(failMessage)
-                
+            let [score, highScore] = [
+                Number(scoreMessage.embeds[0].title.split("\`")[1]),
+                Number(highScoreMessage.embeds[0].title.split("\`")[1])
+            ]
+
+            let [firstMessage, highScoreFirstMessage] = [];
+
+            try {
+                firstMessage = await linkToMessage(scoreMessage.embeds[0].description.slice(7,-1), this.client)
+            } catch {
+                firstMessage = message
             }
 
-            if(message.channel.id !== prism.guild.channelIDs.wording || message.author.bot) return;
+            try {
+                highScoreFirstMessage = await linkToMessage(highScoreMessage.embeds[0].description.slice(7,-1), this.client)
+            } catch {
+                highScoreFirstMessage = firstMessage
+            }
+
+
+            // The Game
 
             const messages = await message.channel.messages.fetch({
 
                 limit: 100
             })
-
-            let firstMessage;
-            let highScoreFirstMessage;
-
-            const scoreMessage = await linkToMessage('https://discordapp.com/channels/447504770719154192/740196706058108958/740558584940593182', this.client)
-            const highScoreMessage = await linkToMessage('https://discordapp.com/channels/447504770719154192/740196706058108958/740557623845322752', this.client)
-
-            let score = Number(scoreMessage.embeds[0].title.split("\`")[1])
-            firstMessage = await linkToMessage(scoreMessage.embeds[0].description.slice(7,-1), this.client)
-
-            let highScore = Number(highScoreMessage.embeds[0].title.split('\`')[1])
-            highScoreFirstMessage = await linkToMessage(highScoreMessage.embeds[0].description.slice(7,-1), this.client)
-
             const lastMessage = messages.filter(m => !m.author.bot).array()[1]
-            let messagesArray = messages.map(m => m.content.toLowerCase())
-            messagesArray.shift()
-            messagesArray.splice(messagesArray.findIndex(m => m.startsWith('<')), 100)
+            let msgArray = messages.map(m => m.content.toLowerCase().trim()).splice(1)
+            msgArray.splice(msgArray.findIndex(m => m.startsWith('<')))
 
-            let increasedScore = 0
+            function fail() {
+
+                const failMessage = score >= highScore ? `You failed, **NEW HIGH SCORE:** \`${score}\`` : `You failed, **SCORE:** \`${score}\``
+                message.reply(failMessage)
+                score = 0
+            }
+
+            let scoreDiff = 0
 
             if(/^[a-z]{1,}$/i.test(message.content) === false || message.author.id === lastMessage.author.id) {
                 message.delete()
+
             } else {
 
+                // Not a word
                 if(!words[message.content.toLowerCase()]) {
-
-                    fail(message, score, highScore)
-                    score = 0;
-
-                } else if(messagesArray.length > 0) {
+                    fail();
                 
-                    if(messagesArray[0].trim().toLowerCase().charAt(messagesArray[0].length - 1) !== message.content.trim().toLowerCase().charAt(0)) {
+                } else if(msgArray.length > 0) {
 
-                        fail(message, score, highScore)
-                        score = 0;
+                    // No Match
+                    if(msgArray[0].charAt(msgArray[0].length - 1) !== message.content.toLowerCase().trim().charAt(0)) {
+                        fail()
+                    // Repeat
+                    } else if(msgArray.includes(message.content.trim().toLowerCase())) {
+                        fail()
 
-                    } else if(messagesArray.includes(message.content.toLowerCase())) {
-
-                        fail(message, score, highScore)
-                        score = 0;
-
+                    // Pass
                     } else {
 
-                        let multiplier = 1
-    
+                        let mult = 1;
+
                         if(lastMessage.content.trim().toLowerCase().split("").reverse().join("") == message.content.trim().toLowerCase()) {
-    
+
+                            mult = 2;
                             message.react('🔁');
-                            multiplier++
-                        
+
                         } else if(alphabetical(message.content) === alphabetical(lastMessage.content)) {
 
-                            message.react('🔀'),
-                            multiplier++
+                            mult = 2;
+                            message.react('🔀');
                         }
 
-                        increasedScore += Math.floor(multiplier * message.content.length / 3);
+                        scoreDiff += Math.floor( mult * message.content.length / 3 );
+                        //(scoreDiff > 10) && (scoreDiff = 10)
 
-                        if(increasedScore > 10) increasedScore = 10;
+                        message.react(emoji.characters[scoreDiff]);
 
-                        message.react(emoji.characters[increasedScore]);
-
-                        if((score += increasedScore) > highScore) {
-                            highScore = score += increasedScore
-                            highScoreFirstMessage = firstMessage
-                        }
                     }
 
                 } else {
 
-                    increasedScore++;
-                    message.react(emoji.characters[increasedScore])
-                    firstMessage = message
-                    score+=increasedScore;
+                    scoreDiff++;
+                    firstMessage = message;
+                    message.react(emoji.characters[scoreDiff]);
                 }
-
-                scoreMessage.edit({ embed: {
-
-                    type: 'rich',
-                    title: `**CURRENT SCORE**: \`${score}\``,
-                    description: `[Jump](${firstMessage.url})`
-                }})
-                highScoreMessage.edit({ embed: {
-
-                    type: 'rich',
-                    title: `**HIGHEST SCORE**: \`${highScore}\``,
-                    description: `[Jump](${highScoreFirstMessage.url})`
-                }})
             }
 
+            // Comparing scores
+
+            score += scoreDiff;
+
+            if(score >= highScore) {
+                highScore = score;
+                highScoreFirstMessage = firstMessage
+            }
+
+            // Editing the Scoreboards
+
+            scoreMessage.edit('', { embed: {
+
+                title: `CURRENT SCORE: \`${score}\``,
+                description: `[Jump](${firstMessage.url})`
+            }})
+            highScoreMessage.edit('', { embed: {
+
+                title: `HIGHEST SCORE: \`${highScore}\``,
+                description: `[Jump](${highScoreFirstMessage.url})`
+            }})
+
         } catch(e) {
+
             console.log(e)
         }
     }
