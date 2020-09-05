a7027e14c4320b80c01e4961375605eb706b4a11
Jacob <jacob.ted.seaman@gmail.com>
2020-09-05 01:09:26 +0100

    Update xpVoice.js

--- listeners/XP/xpVoice.js
+++ listeners/XP/xpVoice.js
@@ -1,5 +1,6 @@
 const { Listener } = require('discord-akairo');
 const { rng } = require('../../functions')
+const { colors, xpArray, prism } = require('../../config')
 
 class XPVoiceListener extends Listener {
     constructor() {
@@ -23,18 +24,21 @@ class XPVoiceListener extends Listener {
 
                 if(newID === afkID) return;
 
-                const data = (await DB.query(`SELECT voice FROM tbl_users WHERE user_id = ${newState.member.id}`)).rows
+                let data = (await DB.query(`SELECT voice, xp, level FROM tbl_users WHERE user_id = ${newState.member.id}`)).rows[0]
 
                 let isVoice;
 
-                if(data[0]) {
-                    isVoice = data[0].voice
+                if(data) {
+                    isVoice = data.voice
                 } else {
+
                     await DB.query(`INSERT INTO tbl_users (user_id, messages, xp, last_message_timestamp, level, voice, voice_minutes) VALUES (${newState.member.id}, 0, 0, 0, 0, false, 0)`, (err, res) => {
 
                         if(err) return console.log(err);
                         console.log(`Added ${newState.member.user.tag} to tbl_users with user_id ${newState.member.id}`)
                     })
+
+                    data = (await DB.query(`SELECT voice FROM tbl_users WHERE user_id = ${newState.member.id}`)).rows[0]
                 }
 
                 if(newState.channel && !isVoice) {
@@ -54,11 +58,32 @@ class XPVoiceListener extends Listener {
                                     if(member.voice.channel.members.size > 1 && member.voice.channel.id !== afkID) {
 
                                         const xpAdd = rng(7,3)
+
+                                        data = (await DB.query(`SELECT voice, xp, level FROM tbl_users WHERE user_id = ${newState.member.id}`)).rows[0]
                                         
                                         await DB.query(`UPDATE tbl_users SET xp = xp + ${xpAdd}, voice_minutes = voice_minutes + 5 WHERE user_id = ${member.id}`, async (err, res) => {
 
                                             if(err) return console.log(err);
                                         })
+                
+                                        if(xpArray[data.level + 1] <= (data.xp + xpAdd)) {
+                        
+                                            let level = Number(data.level) + 1;
+                        
+                                            (await client.channels.fetch(prism.guild.channelIDs.levelUps)).send({
+                                                embed: {
+                                                    title: 'LEVEL UP!',
+                                                    description: `${member.user} you reached level ${level}! <:pogchamp:519201541274730496>`,
+                                                    timestamp: new Date(),
+                                                    footer: {
+                                                        text: member.user.tag
+                                                    },
+                                                    color: colors.purple
+                                                }
+                                            })
+
+                                            await DB.query(`UPDATE tbl_users SET level = ${level} WHERE user_id = ${member.id}`)
+                                        }
                                     }
 
                                     addXP(client)
