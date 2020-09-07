f14dfe0348d2f893cc68285163c8331bef1decfc
Jacob <jacob.ted.seaman@gmail.com>
2020-09-07 21:02:10 +0100

    Create wordingAdd.js

---
+++ commands/Fun/Games/wordingAdd.js
@@ -0,0 +1,36 @@
+const { Command } = require('discord-akairo');
+const fs = require('fs')
+
+class WordingAddCommand extends Command {
+    constructor() {
+        super('addWord', {
+            aliases: ['addword'],
+            ownerOnly: true,
+            args: [
+                {
+                    id: 'word',
+                    match: 'rest'
+                }
+            ]
+        })
+    }
+
+    async exec(message, args) {
+
+        try{
+
+            const file = './listeners/Fun/Games/words_dictionary.json'
+
+            let dictionary = JSON.parse(fs.readFileSync(file))
+
+            dictionary[args.word] = 1
+
+            fs.writeFileSync(file, JSON.stringify(dictionary, null, 2))
+
+            await message.channel.send(`***Added ${args.word} to the dictionary.***`)
+
+        } catch(e) {console.log(e)}
+    }
+}
+
+module.exports = WordingAddCommand;
