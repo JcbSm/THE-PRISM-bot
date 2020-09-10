b70e7e5967a74f73d5c26d357c703c46c81d7b33
Jacob <jacob.ted.seaman@gmail.com>
2020-09-11 00:33:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -183,55 +183,8 @@ module.exports =
                 roleIDs: {
 
                     speaker: '627829247339266048',
+                    main: '447508711855095808'
                 },
-
-                selfRoles: [
-                    {
-                        name: "Film Nights",
-                        id: "578840281005424640",
-                        category: null
-                    },
-                    {
-                        name: 'Minecraft',
-                        id: '603598424448958468',
-                        category: 'Games'
-                    },
-                    {
-                        name: 'Insurgency',
-                        id: '627532049183670282',
-                        category: 'Games',
-                    },
-                    {
-                        name: 'Garry\'s Mod',
-                        id: '627532097401389057',
-                        category: 'Games'
-                    },
-                    {
-                        name: 'Jackbox Games',
-                        id: '627602465210105896',
-                        category: 'Games'
-                    },
-                    {
-                        name: 'Terraria',
-                        id: '654480769108148225',
-                        category: 'Games'
-                    },
-                    {
-                        name: 'PAYDAY',
-                        id: '702858435481436211',
-                        category: 'Games'
-                    },
-                    {
-                        name: 'CS:GO',
-                        id: '707290818402189452',
-                        category: 'Games'
-                    },
-                    {
-                        name: 'Among Us',
-                        id: '753669334324215884',
-                        category: 'Games'
-                    },                
-                ],
 
                 shop: {
 
