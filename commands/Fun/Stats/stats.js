977d6ed84ed6595e7b378d99f0652b45cbae4d4e
Jacob <jacob.ted.seaman@gmail.com>
2020-09-10 14:25:00 +0100

    Update stats.js

--- commands/Fun/Stats/stats.js
+++ commands/Fun/Stats/stats.js
@@ -32,7 +32,7 @@ class StatsCommand extends Command {
             if(!data) return message.reply(`This user has no data to view.`);
 
             const voiceTime = data.total_voice_minutes >= 120 ? `\`${Math.round((data.total_voice_minutes/60)*10)/10}\` hours` : `\`${data.total_voice_minutes}\` minutes`
-            const mutedTime = data.total_mute_minutes >= 120 ? `\`${Math.round((data.total_vmute_minutes/60)*10)/10}\` hours` : `\`${data.total_mute_minutes}\` minutes`
+            const mutedTime = data.total_mute_minutes >= 120 ? `\`${Math.round((data.total_mute_minutes/60)*10)/10}\` hours` : `\`${data.total_mute_minutes}\` minutes`
 
             message.channel.send({embed: {
 
