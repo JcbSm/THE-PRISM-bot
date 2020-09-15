9346e516ef82eff322371ba96528f80d46a5ab06
Jacob <jacob.ted.seaman@gmail.com>
2020-09-16 00:25:32 +0100

    Update levels.js

--- commands/Fun/XP/levels.js
+++ commands/Fun/XP/levels.js
@@ -23,7 +23,7 @@ class LevelsCommand extends Command {
 
             const DB = this.client.db;
 
-            let sent = await message.channel.send('***Calculating...***')
+            let sent = this.client.caching ? await message.channel.send('***Calculating...***\n`Due to the bot recently restarting, users are still being cached. This may take some time...`') : await message.channel.send('***Calculating...***')
 
             const arr = (await DB.query(`SELECT * FROM tbl_users WHERE xp > 0`)).rows
 
@@ -33,7 +33,19 @@ class LevelsCommand extends Command {
 
             for(let i = 0; i < arr.length; i++) {
 
-                arr2.push(`**\`${pad(i+1, 2)}.\`** • \`Lvl [${arr[i].level}]\` • <@${arr[i].user_id}> • \`${groupDigits(arr[i].xp)} xp\``)
+                let mention
+
+                if(message.guild.members.cache.has(arr[i].user_id)) {
+                    mention = `<@${arr[i].user_id}>`
+                } else {
+                    try{
+                        mention = (await this.client.users.fetch(arr[i].user_id)).tag
+                    } catch(err) {
+                        mention = `\`Deleted User\``
+                    }
+                }
+
+                arr2.push(`**\`${pad(i+1, 2)}.\`** • \`Lvl [${arr[i].level}]\` • ${mention} • \`${groupDigits(arr[i].xp)} xp\``)
             }
 
             let page;
